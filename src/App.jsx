90d743ae0e56cf545be5e55af827ca078bf8b374
Mu Vonglao <muvonglao@gmail.com>
2024-09-26 09:53:44 +0700

    feat: installed shadcn ui and setup theme token

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 function NavBar() {
   return (
-    <nav className="flex items-center justify-between py-4 px-8 bg-white border-b ">
+    <nav className="flex items-center justify-between py-4 px-8 bg-background border-b border-muted">
       <a href="/" className="text-2xl font-bold">
-        Thomson P<span className="text-green-500">.</span>
+        Thomson P<span className="text-green-400">.</span>
       </a>
       <div className="hidden md:flex space-x-4">
-        <a href="/login" className="px-9 py-2  rounded-full border">
+        <a
+          href="/login"
+          className="px-9 py-2 rounded-full text-foreground border border-foreground hover:border-muted-foreground hover:text-muted-foreground transition-colors"
+        >
           Log in
         </a>
         <a
           href="/signup"
-          className="px-8 py-2 bg-gray-900 text-white rounded-full hover:bg-gray-700 transition-colors"
+          className="px-8 py-2 bg-foreground text-white rounded-full hover:bg-muted-foreground transition-colors"
         >
           Sign up
         </a>
@@ -30,7 +33,7 @@ function HeroSection() {
             Informed, <br />
             Stay Inspired,
           </h1>
-          <p className="text-lg text-gray-500">
+          <p className="text-lg text-muted-foreground">
             Discover a World of Knowledge at Your Fingertips. Your Daily Dose of
             Inspiration and Information.
           </p>
@@ -43,12 +46,12 @@ function HeroSection() {
         <div className="lg:w-1/3 lg:pl-8">
           <h2 className="text-xl font-semibold mb-2">-Author</h2>
           <h3 className="text-2xl font-bold mb-4">Thompson P.</h3>
-          <p className="text-gray-500 mb-4">
+          <p className="text-muted-foreground mb-4">
             I am a pet enthusiast and freelance writer who specializes in animal
             behavior and care. With a deep love for cats, I enjoy sharing
             insights on feline companionship and wellness.
           </p>
-          <p className="text-gray-500">
+          <p className="text-muted-foreground">
             When I&apos;m not writing, I spend time volunteering at my local
             animal shelter, helping cats find loving homes.
           </p>
